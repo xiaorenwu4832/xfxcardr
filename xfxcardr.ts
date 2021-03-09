5761bddc36d41896478de0aabf44b3076d0305f9
Hony <779509583@qq.com>
2021-03-09 16:47:32 +0800

    v0.0.1

--- xfxcardr.ts
+++ xfxcardr.ts
@@ -1,29 +1,29 @@
+/*
+Copyright (C): 2010-2019, Shenzhen Yahboom Tech
 
-/**
- * 使用此文件来定义自定义函数和图形块。
- * 想了解更详细的信息，请前往 https://makecode.microbit.org/blocks/custom
- */
+*/
 
-/**
- * 自定义图形块
- */
-//% weight=5 color=#d7981d icon="\uf53b"
-namespace Xfxcardr {
-    const PCA9685_ADDRESS = 0x40
+//% color="#ECA40D" weight=20 icon="\uf085" block="小飞象智能车"
+namespace SuperBit {
+
+    const PCA9685_ADD = 0x40
     const MODE1 = 0x00
     const MODE2 = 0x01
     const SUBADR1 = 0x02
     const SUBADR2 = 0x03
     const SUBADR3 = 0x04
-    const PRESCALE = 0xFE
+
     const LED0_ON_L = 0x06
     const LED0_ON_H = 0x07
     const LED0_OFF_L = 0x08
     const LED0_OFF_H = 0x09
+
     const ALL_LED_ON_L = 0xFA
     const ALL_LED_ON_H = 0xFB
     const ALL_LED_OFF_L = 0xFC
     const ALL_LED_OFF_H = 0xFD
+
+    const PRESCALE = 0xFE
 
     const STP_CHA_L = 2047
     const STP_CHA_H = 4095
@@ -39,10 +39,64 @@ namespace Xfxcardr {
 
     let initialized = false
 
+    export enum enSteppers {
+        B1 = 0x1,
+        B2 = 0x2
+    }
+    export enum enPos { 
+        //% blockId="forward" block="正转"
+        forward = 1,
+        //% blockId="reverse" block="反转"
+        reverse = 2,
+        //% blockId="stop" block="停止"
+        stop = 3
+    }
+
+    export enum enTurns {
+        //% blockId="T1B4" block="1/4"
+        T1B4 = 90,
+        //% blockId="T1B2" block="1/2"
+        T1B2 = 180,
+        //% blockId="T1B0" block="1"
+        T1B0 = 360,
+        //% blockId="T2B0" block="2"
+        T2B0 = 720,
+        //% blockId="T3B0" block="3"
+        T3B0 = 1080,
+        //% blockId="T4B0" block="4"
+        T4B0 = 1440,
+        //% blockId="T5B0" block="5"
+        T5B0 = 1800
+    }
+    
+    export enum enServo {
+        
+        S1 = 0,
+        S2,
+        S3,
+        S4,
+        S5,
+        S6,
+        S7,
+        S8
+    }
+    export enum enMotors {
+        M1 = 8,
+        M2 = 10,
+        M3 = 12,
+        M4 = 14
+    }
+
     function i2cwrite(addr: number, reg: number, value: number) {
         let buf = pins.createBuffer(2)
         buf[0] = reg
         buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
         pins.i2cWriteBuffer(addr, buf)
     }
 
@@ -53,12 +107,8 @@ namespace Xfxcardr {
     }
 
     function initPCA9685(): void {
-        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
+        i2cwrite(PCA9685_ADD, MODE1, 0x00)
         setFreq(50);
-        setPwm(0, 0, 4095);
-        for (let idx = 1; idx < 16; idx++) {
-            setPwm(idx, 0, 0);
-        }
         initialized = true
     }
 
@@ -69,59 +119,238 @@ namespace Xfxcardr {
         prescaleval /= freq;
         prescaleval -= 1;
         let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
-        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
+        let oldmode = i2cread(PCA9685_ADD, MODE1);
         let newmode = (oldmode & 0x7F) | 0x10; // sleep
-        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
-        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
-        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
+        i2cwrite(PCA9685_ADD, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADD, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADD, MODE1, oldmode);
         control.waitMicros(5000);
-        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+        i2cwrite(PCA9685_ADD, MODE1, oldmode | 0xa1);
     }
 
     function setPwm(channel: number, on: number, off: number): void {
         if (channel < 0 || channel > 15)
             return;
-
+        if (!initialized) {
+            initPCA9685();
+        }
         let buf = pins.createBuffer(5);
         buf[0] = LED0_ON_L + 4 * channel;
         buf[1] = on & 0xff;
         buf[2] = (on >> 8) & 0xff;
         buf[3] = off & 0xff;
         buf[4] = (off >> 8) & 0xff;
-        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+        pins.i2cWriteBuffer(PCA9685_ADD, buf);
     }
 
-	/**
-	 * Servo Execute
-	 * @param degree [0-180] degree of servo; eg: 90, 0, 180
-	*/
-    //% blockId=setServo block="设置舵机 %channel|角度 %degree"
-    //% weight=85
-    //% degree.min=0 degree.max=180
-    export function Servo(channel: number,degree: number): void {
-		if (!initialized) {
-            initPCA9685();
+    function setStepper(index: number, dir: boolean): void {
+        if (index == enSteppers.B1) {
+            if (dir) {
+                setPwm(11, STP_CHA_L, STP_CHA_H);
+                setPwm(9, STP_CHB_L, STP_CHB_H);
+                setPwm(10, STP_CHC_L, STP_CHC_H);
+                setPwm(8, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(8, STP_CHA_L, STP_CHA_H);
+                setPwm(10, STP_CHB_L, STP_CHB_H);
+                setPwm(9, STP_CHC_L, STP_CHC_H);
+                setPwm(11, STP_CHD_L, STP_CHD_H);
+            }
+        } else {
+            if (dir) {
+                setPwm(12, STP_CHA_L, STP_CHA_H);
+                setPwm(14, STP_CHB_L, STP_CHB_H);
+                setPwm(13, STP_CHC_L, STP_CHC_H);
+                setPwm(15, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(15, STP_CHA_L, STP_CHA_H);
+                setPwm(13, STP_CHB_L, STP_CHB_H);
+                setPwm(14, STP_CHC_L, STP_CHC_H);
+                setPwm(12, STP_CHD_L, STP_CHD_H);
+            }
         }
-		// 50hz: 20,000 us
-        let v_us = (degree * 1800 / 180 + 600); // 0.6 ~ 2.4
-        let value = v_us * 4096 / 20000;
-        setPwm(channel, 0, value);
     }
-	
-	/**
-	 * Servo Execute
-	 * @param pulse [500-2500] pulse of servo; eg: 1500, 500, 2500
-	*/
-    //% blockId=setServoPulse block="设置舵机 %channel|脉宽 %degree"
-    //% weight=85
-    //% pulse.min=500 pulse.max=2500
-    export function ServoPulse(channel: number,pulse: number): void {
-		if (!initialized) {
-            initPCA9685();
-        }
-		// 50hz: 20,000 us
-        let value = pulse * 4096 / 20000;
-        setPwm(channel, 0, value);
-    }
-} 
 
+    function stopMotor(index: number) {
+        setPwm(index, 0, 0);
+        setPwm(index + 1, 0, 0);
+    }
+
+
+    
+    //% blockId=SuperBit_Servo block="舵机(180)|编号 %num|角度 %value"
+    //% weight=97
+    //% blockGap=10
+    //% num.min=1 num.max=4 value.min=0 value.max=180
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=20
+    export function Servo(num: enServo, value: number): void {
+
+        // 50hz: 20,000 us
+        let us = (value * 1800 / 180 + 600); // 0.6 ~ 2.4
+        let pwm = us * 4096 / 20000;
+        setPwm(num, 0, pwm);
+
+    }
+
+    //% blockId=SuperBit_Servo2 block="舵机(270)|编号 %num|角度 %value"
+    //% weight=96
+    //% blockGap=10
+    //% num.min=1 num.max=4 value.min=0 value.max=270
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=20
+    export function Servo2(num: enServo, value: number): void {
+
+        // 50hz: 20,000 us
+        let newvalue = Math.map(value, 0, 270, 0, 180);
+        let us = (newvalue * 1800 / 180 + 600); // 0.6 ~ 2.4
+        let pwm = us * 4096 / 20000;
+        setPwm(num, 0, pwm);
+
+    }
+
+    //% blockId=SuperBit_Servo3 block="舵机(360)|编号 %num|方向 %pos|速度 %value"
+    //% weight=96
+    //% blockGap=10
+    //% num.min=1 num.max=4 value.min=0 value.max=90
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=20
+    export function Servo3(num: enServo, pos: enPos, value: number): void {
+
+        // 50hz: 20,000 us
+        
+        if (pos == enPos.stop) {
+            let us = (86 * 1800 / 180 + 600); // 0.6 ~ 2.4
+            let pwm = us * 4096 / 20000;
+            setPwm(num, 0, pwm);
+        }
+        else if(pos == enPos.forward){ //0-90 -> 90 - 0
+            let us = ((90-value) * 1800 / 180 + 600); // 0.6 ~ 2.4
+            let pwm = us * 4096 / 20000;
+            setPwm(num, 0, pwm);
+        }
+        else if(pos == enPos.reverse){ //0-90 -> 90 -180
+            let us = ((90+value) * 1800 / 180 + 600); // 0.6 ~ 2.4
+            let pwm = us * 4096 / 20000;
+            setPwm(num, 0, pwm);
+        }
+
+       
+
+    }
+    //% blockId=SuperBit_MotorRun block=" 直流电机|%index|速度(-255~255) %speed"
+    //% weight=93
+    //% speed.min=-255 speed.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRun(index: enMotors, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        speed = speed * 16; // map 255 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= -4096) {
+            speed = -4095
+        }
+
+        let a = index
+        let b = index + 1
+        
+        if (a > 10)
+        {
+            if (speed >= 0) {
+                setPwm(a, 0, speed)
+                setPwm(b, 0, 0)
+            } else {
+                setPwm(a, 0, 0)
+                setPwm(b, 0, -speed)
+            }
+        }
+        else { 
+            if (speed >= 0) {
+                setPwm(b, 0, speed)
+                setPwm(a, 0, 0)
+            } else {
+                setPwm(b, 0, 0)
+                setPwm(a, 0, -speed)
+            }
+        }
+        
+    }
+    
+
+
+    //% blockId=SuperBit_MotorRunDual block=" 直流电机|%motor1|速度(-255~255) %speed1|%motor2|速度(-255~255) %speed2"
+    //% weight=92
+    //% blockGap=50
+    //% speed1.min=-255 speed1.max=255
+    //% speed2.min=-255 speed2.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=2
+    export function MotorRunDual(motor1: enMotors, speed1: number, motor2: enMotors, speed2: number): void {
+        MotorRun(motor1, speed1);
+        MotorRun(motor2, speed2);
+    }
+
+    //% blockId=SuperBit_StepperDegree block="步进电机(28BYJ-48) |%index|角度 %degree"
+    //% weight=90
+    export function StepperDegree(index: enSteppers, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        setStepper(index, degree > 0);
+        degree = Math.abs(degree);
+        basic.pause(10240 * degree / 360);
+        MotorStopAll()
+    }
+
+    //% blockId=SuperBit_MotorStopAll block="停止所有电机"
+    //% weight=91
+    //% blockGap=50
+    export function MotorStopAll(): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        
+        stopMotor(enMotors.M1);
+        stopMotor(enMotors.M2);
+        stopMotor(enMotors.M3);
+        stopMotor(enMotors.M4);
+        
+    }
+
+    //% blockId=SuperBit_StepperTurn block="步进电机(28BYJ-48) |%index|转 %turn|圈数"
+    //% weight=89
+    export function StepperTurn(index: enSteppers, turn: enTurns): void {
+        let degree = turn;
+        StepperDegree(index, degree);
+    }
+
+    //% blockId=SuperBit_StepperDual block=双步进电机(角度) |B1 %degree1| B2 %degree2"
+    //% weight=88
+    export function StepperDual(degree1: number, degree2: number): void {
+        if (!initialized) {
+            initPCA9685()
+        }
+        setStepper(1, degree1 > 0);
+        setStepper(2, degree2 > 0);
+        degree1 = Math.abs(degree1);
+        degree2 = Math.abs(degree2);
+        basic.pause(10240 * Math.min(degree1, degree2) / 360);
+        if (degree1 > degree2) {
+            stopMotor(enMotors.M3);
+            stopMotor(enMotors.M4);
+            basic.pause(10240 * (degree1 - degree2) / 360);
+        } else {
+            stopMotor(enMotors.M1);
+            stopMotor(enMotors.M2);
+            basic.pause(10240 * (degree2 - degree1) / 360);
+        }
+
+        MotorStopAll()
+    }
+
+    //% blockId=SuperBit_PWMOFF block="关闭PWM输出|%index"
+    //% weight=87
+    export function PWMOFF(index: number): void {
+        setPwm(index, 0, 0);
+    }
+
+}
